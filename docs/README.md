f873d10887d249bcd8b82e65dc9d4bf66675ef12
Kodai Matsumoto <nobita.0918@gmail.com>
2023-07-07 01:56:22 +0900

    /.well-known/webfinger エンドポイントに対する処理を実装した

--- docs/README.md
+++ docs/README.md
@@ -1,12 +1,23 @@
-# 用語集
+# ActivityPub サーバを自作する
 
-## Actor
+## 起動方法
+
+```bash
+$ npm i
+$ npm run build
+# pino-pretty で fastify からのログを読みやすく整形する
+$ npm run start | npx pino-pretty
+```
+
+## 用語集
+
+### Actor
 
 ユーザのようなもので、JSON-LD に基づいた一般公開されるドキュメント。
 
-## WebFinger
+### WebFinger
 
-「このユーザーネームのユーザは居る？」という質問に対して返答するエンドポイント。
+ここでは「このユーザーネームのユーザは居る？」という質問に対して返答する役割をもつエンドポイントとなる。
 
 エンドポイントの例: `/.well-known/webfinger?resource=acct:bob@my-example.com`
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import fastify from 'fastify';
 import * as E from 'fp-ts/lib/Either.js';
-import { pipe } from 'fp-ts/lib/function.js';
 import * as t from 'io-ts';
+
+const hostname = 'ap.kodai.vision';
+const account = 'kodai';
 
 const server = fastify({ logger: true });
 
@@ -14,14 +16,38 @@ const WebFingerQuery = t.type({
     resource: t.string,
 });
 
-server.get('/.well-known/webfinger', (request) => {
-    return pipe(
-        WebFingerQuery.decode(request.query),
-        E.fold(
-            () => 'Error occurred',
-            ({ resource }) => `Successful! Specified resource is ${resource}`
-        )
-    );
+server.get('/.well-known/webfinger', (request, reply) => {
+    const decodeResult = WebFingerQuery.decode(request.query);
+
+    if (E.isLeft(decodeResult)) {
+        server.log.error(decodeResult.left);
+        void reply
+            .status(400)
+            .send('The `resource` query parameter required, but not found');
+        return;
+    }
+
+    const { resource } = decodeResult.right;
+
+    if (resource !== `acct:${account}@${hostname}`) {
+        void reply.status(404).send('Account not found');
+        return;
+    }
+
+    void reply
+        // JRD (JSON Resource Descriptor)
+        .type('application/jrd+json')
+        .status(200)
+        .send({
+            subject: `acct:${account}@${hostname}`,
+            links: [
+                {
+                    rel: 'self',
+                    type: 'application/activity+json',
+                    href: 'https://ap.kodai.vision/actor',
+                },
+            ],
+        });
 });
 
 void server.listen({ port: 3000 });
